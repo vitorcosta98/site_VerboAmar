3195fd0599a1c1b255dc2c8a2557c29d873709bf
Vitor Costa <vitorcosta8991@gmail.com>
2026-06-28 20:38:09 -0300

    Alteracao funcoes (#63)
    * feat: Ajuste da página de cadastro de turma
    
    * feat: alteração da função de cadastro de turma e interface de cadastro de turma
    
    * feat: alteração no cadastro das turmas
    extraindo informações da página
    extraindo lista de informações dos alunos
    teste para salvar no db de professores feito com sucesso
    
    * feat: criação de verificação se o professor já tem cadastro
    Salva informações se o professor não está cadastrado
    
    * feat: Ajuste na função de cadastro de turma
    Implementação da verificação de dados de alunos
    Cadastro e extração de informações do db de alunos
    Função de cadastro de turma concluída
    
    * fix: correção no loop que acessa os dados dos alunos
    Os nomes dos alunos não eram acessados, então não era possível salvar as informações no banco de dados
    
    * fix: Correção da função de cadastro de turmaas
    * Atualização para cadastrar professor e extrair ID
    * Atualização para cadastrar aluno e extrair ID
    * Correção da função que extrai o ID do aluno
    
    * feat: Função de cadastro de turma
    Função totalmente operacional
    
    * feat: Criação da função de aniversariantes
    Criação da função que carrega os aniversariantes
    Personalização da página home para exibir o nome dos aniversariantes
    
    * teste
    
    * enhance: Aperfeiçoamento
    * Add igitignore
    * Alteração do valor de sexo no formulário de cadastro de usuário
    * Atualização do arquivo de banco de dados
    
    * enhance: removendo arquivos pyc
    
    * enhance: Alteração de variáveis
    * Alteração da variável de sexo no formcriarconta
    *
    * Cadastro de turma realizado: salva informação no banco de dados turmas
    
    * enhance: Inserção de elementos de privilégio
    * main.css: Customização de elementos para cada página, em específico página área acadêmica e turmas;
    * pag_turmas: inserção de botão de excluir turma;
    * area_academica: inserção de botão para excluir atividade;
    
    * enhance: Criação de função para deletar atividades e turmas
    
    * enhance: Funções de deleção
    * Delete de atividade: Deleta tanto a atividade como todas as turmas da atividade;
    * Delete de turma: Deleta a turma com o nome específico
    
    * feat: Exclusão de alunos funcionando
    * Atualiza tabela Alunos
    * Atualiza tabela Turmas

--- site_verbo_amar/routes.py
+++ site_verbo_amar/routes.py
@@ -275,13 +275,10 @@ def carregar_chamada(nome_turma):
     if type(turma.id_aluno) == int:
         lista_id_alunos = [turma.id_aluno]
     else:
-        lista_id_alunos = list(turma.id_aluno)
-        for id in lista_id_alunos:
-            if id == ";":
-                lista_id_alunos.remove(id)
+        lista_id_alunos = [id for id in turma.id_aluno.split(";") if id]
+    
 
     lista_nomes_alunos = carregar_nomes_alunos(lista_id_alunos)
-
     form_chamada = FormChamada()
 
     if form_chamada.validate_on_submit() and "botao_submit_chamada" in request.form:
@@ -448,10 +445,30 @@ def excluir_turma(nome_turma,atividade):
         database.session.execute(excluir_turma)
         database.session.commit()
     except Exception as e:
-        flash("Ocorreu um erro ao excluir a Atividade, recarregue a página")
-        return redirect(url_for("exibir_turmas"))
+        flash("Ocorreu um erro ao excluir a Atividade, recarregue a página.")
+        return redirect(url_for("exibir_turmas", nome=atividade))
 
     flash(f'{nome_turma} excluída com sucesso!', 'alert-success')
 
     return redirect(url_for("exibir_turmas", nome=atividade))
 
+
+@app.route('/area-academica/turmas/chamada/excluir/<turma>/<nome_aluno>/<id_aluno>', methods=['GET','POST'])
+def excluir_aluno(turma, nome_aluno,id_aluno):
+    turma=turma
+    try:
+        excluir_aluno = Aluno.query.filter_by(id=id_aluno).first()
+        database.session.delete(excluir_aluno)
+    
+        turma_aluno = Turma.query.filter_by(nome_turma=turma).first()
+        ids = turma_aluno.id_aluno.split(";")
+        excluir_id_aluno = [id for id in ids if id != str(id_aluno)]
+        turma_aluno.id_aluno = ";".join(excluir_id_aluno)
+        database.session.commit()
+
+    except Exception as e:
+        flash("Ocorreu um erro ao excluir Aluno, recarregue a página", 'alert-danger')
+        return redirect(url_for("carregar_chamada", nome_turma=turma))
+
+    flash(f"Aluno {nome_aluno} excluído com sucesso.", 'alert-success')
+    return redirect(url_for("carregar_chamada", nome_turma=turma))
